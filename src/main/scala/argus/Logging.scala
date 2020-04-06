3ea76e2cb7d0784c0e75da19840486c0a0f61a4e
nir haviv <nirhaviv@gmail.com>
2020-04-06 20:02:38 +0300

    Added logging to app

---
+++ src/main/scala/argus/Logging.scala
@@ -0,0 +1,7 @@
+package argus
+
+import org.apache.logging.log4j.LogManager
+
+trait Logging {
+  lazy val logger = LogManager.getLogger(this)
+}
--- src/main/scala/argus/Main.scala
+++ src/main/scala/argus/Main.scala
@@ -4,14 +4,12 @@ import argus.conf.Plain
 import argus.persistence.Redis
 import argus.server.{EntityHandler, Server}
 
-object Main {
+object Main extends Logging {
+
   def main(args: Array[String]): Unit = {
+    logger.info("The program has started")
     val redis = Redis(Plain.url, Plain.port)
     val handler = new EntityHandler(redis)
     Server.start(handler)
-
-    // Get argus.conf
-    // set up argus.server dependencies
-    // start argus.server
   }
 }
